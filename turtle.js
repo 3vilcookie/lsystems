b114ed845e323194f908cc0fda63210c9c24ed74
Raphael Pour <info@raphaelpour.de>
2018-05-06 09:07:41 +0200

    Turtle throws now exceptions instead of only log it to the console. Improves turtle drawing, it now can be printed as whole in the canvas if the length is choosen right. Adds License header to index.html.

--- turtle.js
+++ turtle.js
@@ -6,17 +6,16 @@
  */
 
 class Turtle {
-    constructor(context, alpha = 60.0, length = 1.0) {
+    constructor(context, alpha = 60.0, n = 1, length = 1.0) {
         this.alpha = alpha;
         this.length = length;
         this.angle = 0.0;
         this.x = 0;
         this.y = 0;
+        this.n = n;
         this.xOffset = 0;
         this.yOffset = 0;
-        this.alphaOffset = 0;
         this.coordinateStack = [];
-
         if (context == null)
             throw new Error("Invalid Context: null");
 
@@ -33,37 +32,71 @@ class Turtle {
         this._alpha = value * Math.PI / 180.0;
     }
 
+    set angleOffset(value){
+        this.angle = value *Math.PI/180.0;
+    }
+
     log() {
         console.log("[Turtle] x:" + this.x + " y:" + this.y + " angle:" + this.alpha + " length:" + this.length);
+    }
+
+    setOrigin(x, y) {
+        this.context.translate(x, y);
+    }
+
+    scale(x, y) {
+        this.context.scale(x, y);
+    }
+
+    rotate(angle) {
+        this.context.rotate(angle);
+    }
+
+    transform(x, y, sx, sy) {
+        this.context.transform(sx, 0, sy, 0, x, y);
     }
 
     forward(length = this.length) {
         var lastX = this.x;
         var lastY = this.y;
 
-        //this.x += this.context.canvas.width* length*length  * Math.sin(this.angle);
-        // this.y += this.context.canvas.height*length*length * Math.cos(this.angle) ;
+        var w = this.context.canvas.width / Math.pow(this.length,this.n);
+        var h = this.context.canvas.height / Math.pow(this.length,this.n);
+        this.x += w * Math.sin(this.angle);
+        this.y += h * Math.cos(this.angle);
 
-        this.x += length * Math.sin(this.angle);
-        this.y += length * Math.cos(this.angle);
+
         this.context.beginPath();
-        this.context.moveTo(lastX, this.context.canvas.height - lastY);
-        this.context.lineTo(this.x, this.context.canvas.height - this.y);
+        this.context.moveTo(lastX, lastY);
+        this.context.lineTo(this.x, this.y);
         this.context.stroke();
 
     }
 
-    right(alpha = this.alpha) {
-        this.angle += alpha * Math.PI / 180.0;
+    right(alpha = this._alpha) {
+        this.angle += alpha;
     }
 
-    left(alpha = this.alpha) {
+    left(alpha = this._alpha) {
         this.right(-alpha);
     }
 
     clear() {
 
+        // Clear Transformation with Identity Matrix
+        this.context.setTransform(1, 0, 0, 1, 0, 0);
+
+        // Clear everything on the canvas
         this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+
+        // Move the coordinate system to left-bottom-corner
+        this.context.translate(this.context.canvas.width/1.5, this.context.canvas.height/2);
+
+        this.context.rotate(-90* Math.PI / 180.0);
+
+        // Scale it to the current factor
+        this.context.scale(.5, -.5);
+
     }
 
     computeWord(word) {
@@ -71,6 +104,7 @@ class Turtle {
         var i;
         for (i = 0; i < word.length; i++)
             this.consume(word[i]);
+
     }
     consume(symbol) {
         switch (symbol) {
@@ -89,17 +123,14 @@ class Turtle {
                 this.coordinateStack.push([this.x, this.y, this.angle]);
                 break;
             case "]":
-                if (this.coordinateStack.length == 0) {
-                    console.log("[Turtle] Nothing coordinate on the stack. '[' missing?");
-                    break;
-                }
+                if (this.coordinateStack.length == 0)
+                    throw "[Turtle] Nothing coordinate on the stack. '[' missing?";
                 var pos = this.coordinateStack.pop();
                 this.x = pos[0];
                 this.y = pos[1];
                 this.angle = pos[2];
                 break;
             default:
-                //console.log("[Turtle] Ignore symbol '" + symbol + "'");
                 break;
         }
     }
