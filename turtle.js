0643be4f0eb2fc62ea0276b5626651d99eeb562a
Raphael Pour <info@raphaelpour.de>
2018-09-24 12:14:52 +0200

    Adds Pre-Processing Canvas for bettter Turtle debugging Adds LStar Button and a circle as default graphic for better Turtle debugging Turtle class provides its own Canvas instead of using the real ones. Turtle uses now two render passes, where each has its own canvas

--- turtle.js
+++ turtle.js
@@ -2,8 +2,99 @@
  * @file:       turtle.js
  * @brief:      Another Turtle Graphic Class using HTML5-Canvas
  * @author:     Raphael Pour <turtle@raphaelpour.de>
- * @license:    (C) 2018 Raphael Pour LGPL
+ * @license:    (C) 2018 Raphael Pour GPL
  */
+
+class Bounds {
+
+    constructor() {
+        this.xMin = Infinity;
+        this.xMax = -Infinity;
+        this.yMin = Infinity;
+        this.yMax = -Infinity;
+
+        this.decimals = 3;
+    }
+
+    get xMin() {
+        return this._xmin;
+    }
+
+    set xMin(value) {
+        this._xmin = value;
+    }
+
+    get xMax() {
+        return this._xmax;
+    }
+
+    set xMax(value) {
+        this._xmax = value;
+    }
+    get yMin() {
+        return this._ymin;
+    }
+
+    set yMin(value) {
+        this._ymin = value;
+    }
+
+    get yMax() {
+        return this._ymax;
+    }
+
+    set yMax(value) {
+        this._ymax = value;
+    }
+
+    process(x, y) {
+        this.processX(x);
+        this.processY(y);
+    }
+
+    processX(value) {
+        if (value > this.xMax)
+            this.xMax = this.round(value);
+        if (value < this.xMin)
+            this.xMin = this.round(value);
+    }
+
+    processY(value) {
+        if (value > this.yMax)
+            this.yMax = this.round(value);
+        if (value < this.yMin)
+            this.yMin = this.round(value);
+    }
+
+    get xAverage() {
+        return this.round((this.xMax + this.xMin) / 2.0);
+    }
+
+    get yAverage() {
+        return this.round((this.yMax + this.yMin) / 2.0);
+    }
+
+    get xLength() {
+        return Math.abs(this.xMax) - Math.abs(this.xMin);
+    }
+
+    get yLength() {
+        return Math.abs(this.yMax) - Math.abs(this.yMin);
+    }
+
+    round(value)
+    {
+        var factor = Math.pow(10,this.decimals);
+        return Math.round(value*factor)/factor;
+    }
+
+    toString() {
+        return "X: " + this.xMin + "/" + this.xMax + "\n" + 
+            "Y: " + this.yMin + "/" + this.yMax +"\n" + 
+            "LEN: " + this.xLength + "/" + this.yLength +"\n" + 
+            "AVG: " + this.xAverage + "/" + this.yAverage;
+    }
+}
 
 class Turtle {
 
@@ -11,25 +102,97 @@ class Turtle {
      * Command Drawing System using an input string and a char interpreter to draw
      * on a HTML5 Canvas  
      */
-    constructor(context, alpha = 60.0, n = 1, length = 1.0) {
+    constructor(width, height, alpha = 60.0, iterationCount = 1, length = 10.0) {
         this.alpha = alpha;
         this.length = length;
-        this.angle = 0.0;
+        this.width = width;
+        this.height = height;
+        this.angle = 0;
         this.x = 0;
         this.y = 0;
-        this.n = n;
+        this.iterationCount = iterationCount;
         this.xOffset = 0;
         this.yOffset = 0;
-        this.bypassDrawing = false;
+        this.xOffsetUI = 0;
+        this.yOffsetUI = 0;
+        this.preProcessingStage = false;
         this.coordinateStack = [];
-        this.bounds = { "x": { "min": Infinity, "max": -Infinity }, "y": { "min": Infinity, "max": -Infinity } };
+        this.debugColorValue = 0.0;
 
-        if (context == null)
-            throw "Invalid Context: null";
+        this.bounds = new Bounds();
 
-        this.context = context;
+
+        // Create Pre-Processing Canvas
+        this.preProcessingCanvas = document.createElement("canvas");
+        this.preProcessingCanvas.id = "preProcessingCanvasId";
+        this.preProcessingCanvas.width = width;
+        this.preProcessingCanvas.height = height;
+
+        if (this.preProcessingCanvas && this.preProcessingCanvas.getContext) {
+            this.preProcessingContext = this.preProcessingCanvas.getContext('2d');
+            this.preProcessingContext.fillStyle = "rgb(0, 70, 215)";
+            this.preProcessingContext.strokeStyle = "black";
+        }
+        else
+            throw "Invalid Pre-Processing Canvas";
+
+
+        // Create Final Canvas 
+        this.finalCanvas = document.createElement("canvas");
+        this.finalCanvas.id = "finalCanvasId";
+        this.finalCanvas.width = width;
+        this.finalCanvas.height = height;
+
+        if (this.finalCanvas && this.finalCanvas.getContext) {
+            this.finalContext = this.finalCanvas.getContext('2d');
+            this.finalContext.fillStyle = "rgb(190, 20, 10)";
+            this.finalContext.strokeStyle = "black";
+
+        }
+        else
+            throw "Invalid Final Canvas";
+
     }
 
+    get finalCanvas() {
+        return this._finalCanvas;
+    }
+
+    set finalCanvas(value) {
+        this._finalCanvas = value;
+    }
+
+    get preProcessingCanvas() {
+        return this._preProcessingCanvas;
+    }
+
+    set preProcessingCanvas(value) {
+        this._preProcessingCanvas = value;
+    }
+
+    set zoomUI(value) {
+        this._zoomUI = value;
+    }
+
+    get zoomUI() {
+        return this._zoomUI;
+    }
+
+    get yOffsetUI() {
+        return this._yOffsetUI;
+    }
+
+    set yOffsetUI(value) {
+        this._yOffsetUI = value;
+    }
+
+    get xOffsetUI() {
+        return this._xOffsetUI;
+    }
+
+    set xOffsetUI(value) {
+        this._xOffsetUI = value;
+    }
 
     get alpha() {
         return this._alpha * 180.0 / Math.PI;
@@ -48,32 +211,34 @@ class Turtle {
     }
 
 
-
     forward(length = this.length) {
         var lastX = this.x;
         var lastY = this.y;
 
-        var w = this.context.canvas.width / Math.pow(this.length, this.n);
-        var h = this.context.canvas.height / Math.pow(this.length, this.n);
-        this.x += w * Math.sin(this.angle);
-        this.y += h * Math.cos(this.angle);
+        this.x += length * Math.sin(this.angle);
+        this.y += length * Math.cos(this.angle);
 
-        if (this.bypassDrawing) {
-            // Bypass Rendering and calculate min/max coordinates
-            if (this.x > this.bounds.x.max)
-                this.bounds.x.max = this.x
-            else if (this.x < this.bounds.x.min)
-                this.bounds.x.min = this.x
-            if (this.y > this.bounds.y.max)
-                this.bounds.y.max = this.y
-            else if (this.y < this.bounds.y.min)
-                this.bounds.y.min = this.y
+
+        var precision = 100;
+
+        if (this.preProcessingStage) {
+            // Calculate min/max coordinates
+            this.bounds.process(this.x, this.y);
+
+            this.preProcessingContext.beginPath();
+            this.preProcessingContext.moveTo(lastX, lastY);
+            this.preProcessingContext.lineTo(this.x, this.y);
+            this.preProcessingContext.stroke();
         }
         else {
-            this.context.beginPath();
-            this.context.moveTo(lastX, lastY);
-            this.context.lineTo(this.x, this.y);
-            this.context.stroke();
+
+            this.debugColorValue += 0.05;
+
+            this.finalContext.beginPath();
+            this.finalContext.strokeStyle = "rgb(" + this.debugColorValue + "," + this.debugColorValue + "," + this.debugColorValue + ")";
+            this.finalContext.moveTo(lastX, lastY);
+            this.finalContext.lineTo(this.x, this.y);
+            this.finalContext.stroke();
         }
 
     }
@@ -88,17 +253,21 @@ class Turtle {
 
     clear() {
 
-        // Clear Transformation with Identity Matrix
-        this.context.setTransform(1, 0, 0, 1, 0, 0);
+        // Clear Pre-Processing Canvas
+        this.preProcessingContext.save();
+
+        this.preProcessingContext.fillRect(0, 0, this.width, this.height);
+
+        this.preProcessingContext.restore();
+
 
         // Clear everything on the canvas
-        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        this.finalContext.save();
 
-        // Move the coordinate system to the center
-        this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
+        this.finalContext.fillRect(0, 0, this.width, this.height);
 
-        // Scale it to the current factor
-        this.context.scale(.5, -.5);
+        this.finalContext.restore();
+
     }
 
     computeWord(word) {
@@ -107,28 +276,57 @@ class Turtle {
             throw "Word is empty. Check if the L-System is correct."
 
 
+        /*
+         * First Render Pass
+         */
+
         // Bypass drawing and calculate bounds
-        this.bypassDrawing = true;
+        this.preProcessingStage = true;
         var i;
         for (i = 0; i < word.length; i++) {
             this.consume(word[i]);
         }
 
-        console.log(this.bounds);
+        /*
+         * Resize Canvas
+         */
 
-        // Second Pass whith
-        this.bypassDrawing = false;
+        //var xLength = Math.abs(this.bounds.x.max) - Math.abs(this.bounds.x.min);
+        //var yLength = Math.abs(this.bounds.y.max) - Math.abs(this.bounds.y.min);
+
+        var resizeFactor;
+
+        // Use Dimension with the highest extend and stretch it to the
+        // domain [0,1]x[0,1]
+        console.log(this.bounds.toString());
+        if (this.bounds.xLength > this.bounds.yLength)
+            resizeFactor = this.width / this.bounds.xLength;
+        else
+            resizeFactor = this.height / this.bounds.yLength;
+
+
+        /*
+         * Second Render Pass
+         */
+        // Rendering with resized canvas
+        var xOffsetToCenter = this.bounds.xAverage / 2.0;
+        var yOffsetToCenter = this.bounds.yAverage / 2.0;
+
+        this.finalContext.setTransform(1, 0, 0, 1, 0, 0);
+        this.finalContext.scale(resizeFactor, resizeFactor);
+        this.finalContext.translate(xOffsetToCenter, yOffsetToCenter);
+
+        // TODO : Display first rendering pass in order to check if
+        //        the debug circle is inside the canvas
+        //        If yes: fuck, if no, adjust translation
+        this.preProcessingStage = false;
+        //this.clear();
+
         for (i = 0; i < word.length; i++) {
             this.consume(word[i]);
         }
-        /*
-        var width = this.bounds.x.max - this.bounds.x.min;
-        var height = this.bounds.y.max - this.bounds.y.min;
-
-        for (i = 0; i < word.length; i++) 
-            this.consume(word[i]);
-            */
     }
+
     consume(symbol) {
         switch (symbol) {
             case "G":
